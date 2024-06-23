2b5d408429294a9df1692e9d5e628007cb56fc00
Matthew Yu <myu2@umbc.edu>
2024-06-23 17:46:48 -0400

    java project

---
+++ src/FunctionFile.java
@@ -0,0 +1,19 @@
+import java.awt.FileDialog;
+
+public class FunctionFile {
+    GUI gui;
+
+    public FunctionFile(GUI gui) {
+        this.gui = gui;
+    }
+
+    public void newFile() {
+        gui.textArea.setText("");
+        gui.window.setTitle("New");
+    }
+
+    public void open() {
+        FileDialog fd = new FileDialog(gui.window, "Open", FileDialog.LOAD);
+        fd.setVisible(true);
+    }
+}
